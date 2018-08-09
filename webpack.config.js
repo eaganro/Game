6a687b72bc0d4d76a828768fb70f43edcf9358eb
eaganro <rory.eagan@gmail.com>
2018-08-09 00:08:08 -0400

    Add css loaders to webpack. Begin to make grid layout.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }, 
+      {
         test: /\.jsx$/,
         include: __dirname + '/client/dev',
         use: {
